11e7b192a31375ae13aaa2c37bf1b89c71b45594
Fedirino <ferrarinick0@gmail.com>
2026-06-28 17:28:34 -0400

    Show Kyo version and refresh cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'kyo-v0.2.1';
+const CACHE = 'kyo-v0.2.2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png', '/favicon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
